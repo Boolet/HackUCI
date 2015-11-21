4585261dbb266fa91ec333fac4304c1179784da2
Boolet <TrebecS@Gmail.com>
2015-11-21 01:38:22 -0800

    Initial
    Initial commit; cursor controller, intractable object, pickup object

---
+++ CursorSpriteController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CursorSpriteController : MonoBehaviour {
+
+	List<GameObject> insideObjects;
+
+	// Use this for initialization
+	void Start () {
+		insideObjects = new List<GameObject> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (insideObjects.Count > 0) {
+			//interact indication
+		} else {
+			//passive mode
+		}
+	}
+
+	public void CursorOnObject(GameObject onObject){
+		if(!insideObjects.Contains(onObject)){
+			insideObjects.Add(onObject);
+		}
+	}
+
+	public void CursorLeaveObject(GameObject onObject){
+		if(insideObjects.Contains(onObject)){
+			insideObjects.Remove(onObject);
+		}
+	}
+}
---
+++ InteractableObject.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(Collider2D))]
+[RequireComponent (typeof(SpriteRenderer))]
+public class InteractableObject : MonoBehaviour {
+
+	SpriteRenderer collisionArea;
+	CursorSpriteController masterController;
+
+	// Use this for initialization
+	void Start () {
+		collisionArea = GetComponent<SpriteRenderer> ();
+		masterController = GameObject.Find ("Cursor Sprite").GetComponent<CursorSpriteController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnMouseDown(){
+		print ("Yay!");
+		//pass to character, move character, character interacts with object
+	}
+
+	public void OnCharacterInteract(GameObject character){
+
+	}
+
+	void OnMouseEnter(){
+		masterController.CursorOnObject (gameObject);
+	}
+
+	void OnMouseLeave(){
+		masterController.CursorLeaveObject (gameObject);
+	}
+}
